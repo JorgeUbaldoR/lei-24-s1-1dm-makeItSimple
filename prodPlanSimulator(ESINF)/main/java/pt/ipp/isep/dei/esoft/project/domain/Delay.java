0f116aefec262f17e21025946b43ade9b9ba9b75
RomeuZouXu <francpok.zouxu4@gmail.com>
2025-01-05 15:19:03 +0000

    USEI24 documentation

--- prodPlanSimulator(ESINF)/main/java/pt/ipp/isep/dei/esoft/project/domain/Delay.java
+++ prodPlanSimulator(ESINF)/main/java/pt/ipp/isep/dei/esoft/project/domain/Delay.java
@@ -9,24 +9,37 @@ import java.util.Map;
 
 public class Delay {
 
-    public Map<String, Object> calculateOriginalCriticalPath(MapGraph<Activity, Double> createdMap) {
+    /**
+     * Calculates the critical path by calling a method in CriticalPath class
+     *
+     * @param createdMap needed for the calculation of the critical path
+     * @return A map with the critical path
+     */
+    public Map<String, Object> calculateCriticalPath(MapGraph<Activity, Double> createdMap) {
         CriticalPath criticalPath = new CriticalPath();
         return criticalPath.calculateCriticalPath(createdMap);
     }
 
-    public void updateActivityDuration(Activity activity, double delay) {
-        double newDuration = activity.getDuration() + delay;
+    /**
+     * Updates the duration of a Activity
+     *
+     * @param activity to be updated
+     * @param duration of the activity
+     */
+    public void updateActivityDuration(Activity activity, double duration) {
+        double newDuration = activity.getDuration() + duration;
         if (newDuration < 0) {
             throw new IllegalArgumentException("Duration cannot be negative");
         }
         activity.setDuration(newDuration);
     }
 
-    public Map<String, Object> calculateDelayedCriticalPath(MapGraph<Activity, Double> createdMap) {
-        CriticalPath criticalPath = new CriticalPath();
-        return criticalPath.calculateCriticalPath(createdMap);
-    }
-
+    /**
+     * Removes the IDs A-7777 and A-7778 since they are just a placeholder for start and finish
+     *
+     * @param createdMap where the IDs should be removed from
+     * @return a new map where there are no start and finish placeholders
+     */
     public MapGraph<Activity, Double> removeActivities(MapGraph<Activity, Double> createdMap) {
         Iterator<Activity> iterator = createdMap.vertices().iterator();
         ID start = new ID(7777, TypeID.ACTIVITY);
